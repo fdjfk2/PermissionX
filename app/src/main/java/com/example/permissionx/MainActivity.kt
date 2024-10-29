1d8e27a996d44d834f6f2319ee30d576ce042ab8
Zhou <2625968476@qq.ccom>
2024-10-29 14:55:32 +0800

    完成PermissionX库的开发与测试工作。

--- app/src/main/java/com/example/permissionx/MainActivity.kt
+++ app/src/main/java/com/example/permissionx/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.permissionx
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.permissionx.databinding.ActivityMainBinding
+import com.permissionx.fdjfk2.PermissionCallback
+import com.permissionx.fdjfk2.PermissionX
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.makeCallBtn.setOnClickListener{
+            PermissionX.request(this,
+                Manifest.permission.CALL_PHONE){ allGranted, deniedList ->
+                    if(allGranted){
+                        call()
+                    }else{
+                        Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+
+    private fun call(){
+        try{
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        }catch (e:SecurityException){
+            e.printStackTrace()
         }
     }
 }
